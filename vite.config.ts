98cc8295361f4170d4b2507d2d2e974aa7440d67
Matej Ruzicka <matejruzicka@Matejs-MacBook-Pro.local>
2025-05-07 10:16:15 +0100

    fixed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        esbuildOptions: {
-            target: "es2020",
-        },
-    },
-    esbuild: {
-        // https://github.com/vitejs/vite/issues/8644#issuecomment-1159308803
-        logOverride: { "this-is-undefined-in-esm": "silent" },
-    },
-    plugins: [
-        react({
-            babel: {
-                plugins: [
-                    "babel-plugin-macros",
-                    "babel-plugin-styled-components",
-                ],
-            },
-        }),
-    ],
+    plugins: [react(), macrosPlugin()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
